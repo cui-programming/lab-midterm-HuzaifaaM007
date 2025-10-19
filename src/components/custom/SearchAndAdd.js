3fe1273c635b4c294a8f54f4dd9c4219b41a636e
HuzaifaaM007 <huzaifamurtaza007@gmail.com>
2025-10-19 21:36:13 +0500

    done all the tasks
    All the tasks  given in student task.md completed

--- src/components/custom/SearchAndAdd.js
+++ src/components/custom/SearchAndAdd.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import { styles } from '../../styles/styles';
+import { Button, Text, TextInput } from '../ui';
 
 /**
  * Custom/SearchAndAdd
@@ -10,10 +11,49 @@ import { styles } from '../../styles/styles';
  *  - use only components from 'ui' for inputs and buttons
  *  - lifting state up if needed
  */
-export default function SearchAndAdd() {
+export default function SearchAndAdd({ onSearch, onAdd }) {
+
+  const [searchText, setSearchText] = useState('');
+  const [newPhrase, setNewPhrase] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchText.trim().toLowerCase());
+  };
+
+  const handleAdd = () => {
+    const phrase = newPhrase.trim();
+    if (phrase) {
+      onAdd(phrase);
+      setNewPhrase('');
+    }
+  };
+
+
   return (
     <View style={styles.section}>
       {/* TODO: Implement search and add UI here using ui/TextInput and ui/Button */}
+
+      <Text style={styles.sectionTitle}>Search & Add Zikr</Text>
+      <View style={styles.inputbtn}>
+        <TextInput
+          style={styles.input}
+          placeholder="Search Zikr..."
+          value={searchText}
+          onChangeText={setSearchText}
+        />
+        <Button style={styles.button} onPress={handleSearch}>Search</Button>
+      </View>
+
+      <View style={styles.inputbtn}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add New Zikr..."
+          value={newPhrase}
+          onChangeText={setNewPhrase}
+        />
+        <Button style={styles.button} onPress={handleAdd}>Add</Button>
+
+      </View>
     </View>
   );
 }
--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -8,7 +8,7 @@ import { Pressable, Text } from 'react-native';
 export default function Button({ onPress, children, style, ...rest }) {
   return (
     <Pressable onPress={onPress} style={style} {...rest}>
-      <Text>{children}</Text>
+      <Text >{children}</Text>
     </Pressable>
   );
 }
